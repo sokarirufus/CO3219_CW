942c7711603f7dd61a0fc1e1163cabc49972ced3
Tiffany Gachuma <tiffany@Tiffanys-MacBook-Pro.local>
2025-12-04 02:22:25 +0000

    changing config.js

--- web/src/config.js
+++ web/src/config.js
@@ -1,3 +1,2 @@
-// web/src/config.js
-export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:4000'
-export const WS_URL  = import.meta.env.VITE_WS_URL  || 'ws://localhost:4000'
+export const API_URL = 'http://localhost:8080'
+export const WS_URL  = 'ws://localhost:8080'
