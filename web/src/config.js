dc19c776c40d57d0b2045705f08ff744aaa97632
Tiffany Gachuma <tiffany@Tiffanys-MacBook-Pro.local>
2025-12-06 00:59:08 +0000

    fixing config.js

--- web/src/config.js
+++ web/src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:8080'
-export const WS_URL  = 'ws://localhost:8080'
+export const API_URL = 'http://localhost:9000'
+export const WS_URL  = 'ws://localhost:9000'
